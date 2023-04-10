f399d3330f375d6481aa4277bb3b39d59939270a
huzaif itoo <huzaifitoo@gmail.com>
2023-04-10 18:06:28 +0530

    2nd commit

--- app/src/main/java/com/example/fcmapp/MainActivity.kt
+++ app/src/main/java/com/example/fcmapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.fcmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fcmapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
