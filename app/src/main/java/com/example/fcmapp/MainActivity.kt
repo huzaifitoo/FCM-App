f54e617c9a59f05dea7ec9987957a58fb63b0801
huzaif itoo <huzaifitoo@gmail.com>
2023-04-11 09:42:31 +0530

    3rd commit

--- app/src/main/java/com/example/fcmapp/MainActivity.kt
+++ app/src/main/java/com/example/fcmapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.fcmapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.fcmapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +15,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        loadData()
 
+        binding.btnButton.setOnClickListener {
+
+            saveData()
+        }
 
     }
+
+    private fun loadData() {
+
+        val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val savedString = sharedPreferences.getString("STRING_KEY", null)
+        val savedBoolean = sharedPreferences.getBoolean("BOOLEAN_KEY",false)
+
+        binding.tvText.text = savedString
+        binding.swSwitch.isChecked = savedBoolean
+    }
+
+    private fun saveData() {
+
+        val insertedText = binding.etText.text.toString()
+        binding.tvText.text = insertedText
+
+        val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        editor.apply{
+            putString("STRING_KEY" , insertedText)
+            putBoolean("BOOLEAN_KEY", binding.swSwitch.isChecked)
+        }.apply()
+
+        Toast.makeText(this, "Data Saved", Toast.LENGTH_SHORT).show()
+    }
+
+
+
 }
